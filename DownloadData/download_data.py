f84c5f429ec4c4e82225e4d0eef6075675dc7327
Niklas Daecke <niklas.daecke@mailbox.org>
2025-11-03 23:40:16 +0100

    Move download_data to Jupyter Notebook

--- DownloadData/download_data.py
+++ DownloadData/download_data.py
@@ -1,17 +1,7 @@
 from datasets import load_dataset
 
 # Download/Loading of the dataset
-dataset_name = "DamianBoborzi/car_images"
-print("\n START DOWNLOAD \n")
-try:
-    dataset = load_dataset(dataset_name)
 
-    # Zeige die Struktur des Datensatzes an
-    print("Datensatz erfolgreich geladen! \n")
-except Exception as e:
-    print(f"Fehler beim Laden des Datensatzes: {e}")
 
 
 # Examining the data set
-print(dataset, "\n")
-print(dataset["train"][0], "\n")
